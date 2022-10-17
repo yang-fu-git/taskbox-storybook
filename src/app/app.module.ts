5b1dfc593ef55b06fba124b0167915f172cfb826
YangFu <yang.3.fu@continental-corporation.com>
2022-10-17 09:27:13 +0200

    Data part, try to fix the bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task.component';
-import { Task } from './task.module';
+// wire the store to the app.
 
 @NgModule({
-  declarations: [AppComponent, TaskComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+            TaskModule,
+             NgxsModule.forRoot([], {
+              developmentMode: !environment.production,
+            }),
+            NgxsReduxDevtoolsPluginModule.forRoot(),
+            NgxsLoggerPluginModule.forRoot({
+              disabled: environment.production,
+            }),
+            ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,69 +1,39 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngxs/store'; // read data from the store
+import { ArchiveTask, PinTask } from '../state/task.state';
+import { Observable } from 'rxjs';
 
-import { Task } from '../task.module';
 
+// keep the presentational version of the TaskList separate is that it is easier to test and isolate.
+// As it doesn't rely on the presence of a store, it is much easier to deal with from a testing perspective.
 @Component({
   selector: 'app-task-list',
   template: `
-    <div class="list-items">
-      <app-task
-        *ngFor="let task of tasksInOrder"
-        [task]="task"
-        (onArchiveTask)="onArchiveTask.emit($event)"
-        (onPinTask)="onPinTask.emit($event)"
-      >
-      </app-task>
-      <div
-        *ngIf="tasksInOrder.length === 0 && !loading"
-        class="wrapper-message"
-      >
-        <span class="icon-check"></span>
-        <p class="title-message">You have no tasks</p>
-        <p class="subtitle-message">Sit back and relax</p>
-      </div>
-      <div *ngIf="loading">
-        <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
-          <span class="glow-checkbox"></span>
-          <span class="glow-text">
-            <span>Loading</span> <span>cool</span> <span>state</span>
-          </span>
-        </div>
-      </div>
-    </div>
-    `,
+    <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
+  `,
 })
 export class TaskListComponent {
-  /** The list of tasks */
+  tasks$?: Observable<any>;
+
+  constructor(private store: Store) {
+     this.tasks$ = store.select((state) => state.taskbox.tasks);
+  }
+
   /**
-+  * @ignore
-+  * Component property to define ordering of tasks
-+  */
- tasksInOrder: Task[] = [];
+   * Component method to trigger the archiveTask event
+   */
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
 
-  /** Checks if it's in loading state */
-  @Input() loading = false;
-
-  /** Event to change the task to pinned */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onPinTask = new EventEmitter<Event>();
-
-  /** Event to change the task to archived */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onArchiveTask = new EventEmitter<Event>();
- @Input()
-  set tasks(arr: Task[]) {
-   const initialTasks = [
-     ...arr.filter(t => t.state === 'TASK_PINNED'),
-     ...arr.filter(t => t.state !== 'TASK_PINNED'),
-   ];
-   const filteredTasks = initialTasks.filter(
-     t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
-   );
-   this.tasksInOrder = filteredTasks.filter(
-     t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
-   );
-
+  /**
+   * Component method to trigger the pinTask event
+   */
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
   }
 }

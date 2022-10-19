404f36efb7bc1940af24038621c695e51bf739e6
YangFu <yang.3.fu@continental-corporation.com>
2022-10-19 15:53:39 +0200

    Spot UI changes

--- src/app/state/task.state.ts
+++ src/app/state/task.state.ts
@@ -25,7 +25,7 @@ export class PinTask {
 // The initial state of our store when the app loads.
 // Usually you would fetch this from a server
 const defaultTasks = [
-  { id: '1', title: 'Something', state: 'TASK_INBOX' },
+  { id: '1', title: 'Something', state: 'TASK_PINNED' },
   { id: '2', title: 'Something more', state: 'TASK_INBOX' },
   { id: '3', title: 'Something else', state: 'TASK_INBOX' },
   { id: '4', title: 'Something again', state: 'TASK_INBOX' },
